e3ac5f379838c5ec6257256ec6079a322356b529
Andrew Straw <strawman@astraw.com>
2014-02-22 12:30:53 +0100

    fixup setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup, find_packages
-import sys,os
+from setuptools import setup
 
 setup(name='motmot.fmf_saver',
-    description='FMF saver plugin for FView',
-    version='0.0.1',
-    packages = find_packages(),
-    author='Andrew Straw',
-    author_email='strawman@astraw.com',
-    url='http://code.astraw.com/projects/motmot',
-    entry_points = {
-  'motmot.fview.plugins':'fmf_saver = motmot.fmf_saver.fmf_saver:FmfSaver',
-  },
-    )
+      description='FMF saver plugin for FView',
+      version='0.0.1',
+      packages = find_packages(),
+      packages = ['motmot', 'motmot.fmf_saver'],
+      namespace_packages = ['motmot'],
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      url='http://code.astraw.com/projects/motmot',
+      entry_points = {
+    'motmot.fview.plugins':'fmf_saver = motmot.fmf_saver.fmf_saver:FmfSaver',
+    },
+      )
